ae0e5603b2c4eeab73b1c14330469d55cbebf391
Vladimir Rudnyh <dreadatour@gmail.com>
2023-05-30 18:45:56 +0700

    Restart proxy in case of -1004 error

--- just_audio/lib/just_audio.dart
+++ just_audio/lib/just_audio.dart
@@ -847,6 +847,16 @@ class AudioPlayer {
       return duration;
     } on PlatformException catch (e) {
       try {
+        // cant connect to servers
+        if (e.code == "-1004" && source is LockCachingAudioSource) {
+          // proxy is offline
+          try {
+            await _proxy._server.close(force: true);
+          } catch (_) {
+            // ignore err
+          }
+          await _proxy.start();
+        }
         throw PlayerException(int.parse(e.code), e.message);
       } on FormatException catch (_) {
         if (e.code == 'abort') {
@@ -2000,18 +2010,28 @@ class _ProxyHttpServer {
   /// Starts the server.
   Future<dynamic> start() async {
     _running = true;
-    _server = await HttpServer.bind(InternetAddress.loopbackIPv4, 0);
-    _server.listen((request) async {
-      if (request.method == 'GET') {
-        final uriPath = _requestKey(request.uri);
-        final handler = _handlerMap[uriPath]!;
-        handler(this, request);
-      }
-    }, onDone: () {
+    try {
+      _server = await HttpServer.bind(InternetAddress.loopbackIPv4, 0);
+      _server.listen((request) async {
+        if (request.method == 'GET') {
+          final uriPath = _requestKey(request.uri);
+          final handler = _handlerMap[uriPath]!;
+          handler(this, request);
+        }
+      }, onDone: () {
+        _running = false;
+      }, onError: (Object e, StackTrace st) async {
+        _running = false;
+        try {
+          await _server.close(force: true);
+        } catch (_) {
+          // ignore
+        }
+      }, cancelOnError: true);
+    } catch (_) {
+      // ignore
       _running = false;
-    }, onError: (Object e, StackTrace st) {
-      _running = false;
-    });
+    }
   }
 
   /// Stops the server
